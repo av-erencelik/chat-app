bcacae06ec0c8f4e5aa13da7a5632870e0863f4d
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-30 18:54:16 +0300

    sidebar component tree finished

---
+++ src/components/Chats.jsx
@@ -0,0 +1,27 @@
+export default function Chats() {
+  return (
+    <div className="chats">
+      <div className="chatUser">
+        <img src="https://images.pexels.com/photos/14148025/pexels-photo-14148025.jpeg" alt="pp"></img>
+        <div className="nameMessage">
+          <span className="name">Eren</span>
+          <p className="message">Hello!</p>
+        </div>
+      </div>
+      <div className="chatUser">
+        <img src="https://images.pexels.com/photos/14148025/pexels-photo-14148025.jpeg" alt="pp"></img>
+        <div className="nameMessage">
+          <span className="name">Eren</span>
+          <p className="message">Hello!</p>
+        </div>
+      </div>
+      <div className="chatUser">
+        <img src="https://images.pexels.com/photos/14148025/pexels-photo-14148025.jpeg" alt="pp"></img>
+        <div className="nameMessage">
+          <span className="name">Eren</span>
+          <p className="message">Hello!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import Chats from "./Chats";
 import Navbar from "./Navbar";
 import Search from "./Search";
 
@@ -6,6 +7,7 @@ export default function Sidebar() {
     <div className="sidebar">
       <Navbar></Navbar>
       <Search></Search>
+      <Chats></Chats>
     </div>
   );
 }
