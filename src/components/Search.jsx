dbbc2a30150c0dfdf20bc6afeb72f8114faa6f06
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-30 17:22:56 +0300

    created search component for finding a user

---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+export default function Search() {
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a user"></input>
+      </div>
+    </div>
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import Search from "./Search";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <Navbar></Navbar>
+      <Search></Search>
     </div>
   );
 }
