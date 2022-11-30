a7dda3dbe4d323d897466af006d03a788945fca4
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-30 20:04:10 +0300

    all components created and styled without functionality

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,3 +1,32 @@
+import { AiTwotoneVideoCamera, AiOutlineUserAdd } from "react-icons/ai";
+import { BsThreeDots } from "react-icons/bs";
+import { BiImageAdd, BiSend } from "react-icons/bi";
+import { MdAttachFile } from "react-icons/md";
+import Messages from "./Messages";
+
 export default function Chat() {
-  return <div className="chat"></div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span className="name">Eren Çelik</span>
+        <div className="chatIcons">
+          <AiTwotoneVideoCamera></AiTwotoneVideoCamera>
+          <AiOutlineUserAdd></AiOutlineUserAdd>
+          <BsThreeDots></BsThreeDots>
+        </div>
+      </div>
+      <Messages></Messages>
+      <div className="input">
+        <input type="text" placeholder="Type something..."></input>
+        <div className="send">
+          <MdAttachFile className="icon"></MdAttachFile>
+          <input type="file" style={{ display: "none" }} id="file"></input>
+          <label htmlFor="file">
+            <BiImageAdd className="icon"></BiImageAdd>
+          </label>
+          <BiSend className="icon"></BiSend>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Message.jsx
@@ -0,0 +1,14 @@
+export default function Message() {
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/14148025/pexels-photo-14148025.jpeg" alt="pp"></img>
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/14148025/pexels-photo-14148025.jpeg" alt="pp"></img>
+      </div>
+    </div>
+  );
+}
